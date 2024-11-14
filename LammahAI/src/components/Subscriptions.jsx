b691f02b256e750bdcc5e2f854af2665e90b19d0
Mohdoalomar <mohdoalomar@gmail.com>
2024-11-14 04:43:28 +0300

    animation lazy load on subscriptions

--- LammahAI/src/components/Subscriptions.jsx
+++ LammahAI/src/components/Subscriptions.jsx
@@ -1,46 +1,65 @@
 import React from 'react';
-
+import useLazyLoad from '../hooks/useLazyLoad';
 
 const Subscriptions = () => {
- const tiersOnDesktop = [
+  const [firstRef, isFirstVisible] = useLazyLoad({
+    threshold: 0.1,
+    rootMargin: '25px'
+  });
+  const [secondRef, isSecondVisible] = useLazyLoad({
+    threshold: 0.1,
+    rootMargin: '25px'
+  });
+  const [thirdRef, isThirdVisible] = useLazyLoad({
+    threshold: 0.1,
+    rootMargin: '25px'
+  });
+
+  const tiersOnDesktop = [
     {
-        name: "حريف",
-        price: "375 SAR",
-        title: ": كل ما هو متضمن في باقة المبدعين، بالإضافة إلى",
-        features: [
-          "أدوات كتابة متقدمة مع تحليلات",
-          "مجموعة أدوات ما قبل الإنتاج البالغة",
-          "صور عالية الجودة"
-        ],
-        delay:700 ,
-        duration : 1000
-      },
+      name: "حريف",
+      price: "375 SAR",
+      title: "كل ما هو متضمن في باقة المبدعين، بالإضافة إلى",
+      features: [
+        "أدوات كتابة متقدمة مع تحليلات",
+        "مجموعة أدوات ما قبل الإنتاج البالغة",
+        "صور عالية الجودة"
+      ],
+      delay: 700,
+      duration: 1000,
+      ref: firstRef,
+      isVisible: isFirstVisible,
+    },
     {
       name: "رهيب",
       price: "150 SAR",
-      title: ": كل ما هو متضمن في الباقة الأساسية، بالإضافة إلى",
+      title: "كل ما هو متضمن في الباقة الأساسية، بالإضافة إلى",
       features: [
         "أدوات كتابة أساسية بدون قيود",
         "أدوات ما قبل الانتاج الأساسية",
         "طلبات وصور ذكاء اصطناعي غير محدودة"
       ],
       delay: 300,
-      duration : 700
+      duration: 700,
+      ref: secondRef,
+      isVisible: isSecondVisible,
     },
-    
     {
-        name: "هاوي",
-        price: "0 SAR",
-        features: [
-          "أدوات كتابة أساسية",
-          "وصول محدود لميزات تطوير القصة",
-          "طلبات الذكاء الاصطناعي مع فترة",
-          "استخدام لمدة 30 يوما"
-        ],
-        delay: 75,
-        duration : 500
-      }
+      name: "هاوي",
+      price: "0 SAR",
+      features: [
+        "أدوات كتابة أساسية",
+        "وصول محدود لميزات تطوير القصة",
+        "طلبات الذكاء الاصطناعي مع فترة",
+        "استخدام لمدة 30 يوما"
+      ],
+      delay: 75,
+      duration: 500,
+      ref: thirdRef,
+      isVisible: isThirdVisible,
+    }
   ];
+
   const tiersOnMobile = [
     {
       name: "هاوي",
@@ -52,7 +71,9 @@ const Subscriptions = () => {
         "استخدام لمدة 30 يوما"
       ],
       delay: 75,
-      duration : 500
+      duration: 500,
+      ref: firstRef,
+      isVisible: isFirstVisible,
     },
     {
       name: "رهيب",
@@ -64,7 +85,9 @@ const Subscriptions = () => {
         "طلبات وصور ذكاء اصطناعي غير محدودة"
       ],
       delay: 300,
-      duration : 700
+      duration: 700,
+      ref: secondRef,
+      isVisible: isSecondVisible,
     },
     {
       name: "حريف",
@@ -75,46 +98,50 @@ const Subscriptions = () => {
         "مجموعة أدوات ما قبل الإنتاج البالغة",
         "صور عالية الجودة"
       ],
-      delay:700 ,
-      duration : 1000
+      delay: 700,
+      duration: 1000,
+      ref: thirdRef,
+      isVisible: isThirdVisible,
     }
-    
   ];
-  let tiers
-  window.innerWidth > 768 ? tiers = tiersOnDesktop : tiers = tiersOnMobile;
+
+  const tiers =  window.innerWidth > 768 ? tiersOnDesktop : tiersOnMobile;
 
   return (
     <div className="max-w-full mx-auto p-6">
-      <h1 className="text-4xl lg:text-5xl text-center mb-12 text-LammahBrown font-bold lg:animate-delay-1000 animate-fade-up">الدراهم</h1>
-      <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
-      {tiers.map((tier, index) => (
-      <div className={` text-right animate-fade-up animate-duration-${tier.duration} animate-delay-${tier.delay}`}>
-        
-          <div 
-            key={index} 
-            className="rounded-2xl overflow-hidden shadow-lg bg-white/10  text-LammahBrown border border-LammahBrown/10  "
+      <h1 className="text-4xl lg:text-5xl text-center mb-12 text-LammahBrown font-bold lg:animate-delay-1000 animate-fade-up">
+        الدراهم
+      </h1>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+        {tiers.map((tier, index) => (
+          <div
+            key={index}
+            ref={tier.ref}
+            className={`text-right ${tier.isVisible ? `animate-fade-up animate-duration-${tier.duration} animate-delay-${tier.delay}` : 'opacity-0'}`}
           >
-            <div className="p-6">
-              <div className="rounded-full bg-LammahBrown text-white text-center py-2 px-8 mb-6 w-full border border-white">
-                <h2 className="text-xl font-bold">{tier.name}</h2>
-              </div>
-              
-              <ul className="space-y-4 mb-6">
-              <li className="text-base font-semibold" >{tier.title}</li>
-                {tier.features.map((feature, fIndex) => (
-                  <li key={fIndex} className="text-base  list-disc" style={{direction : 'rtl'}}>{feature}</li>
-                ))}
-              </ul>
-              
-              <div className="text-2xl font-bold text-center mt-auto">
-                {tier.price}
+            <div className="rounded-2xl overflow-hidden shadow-lg bg-white/10 text-LammahBrown border border-LammahBrown/10">
+              <div className="p-6">
+                <div className="rounded-full bg-LammahBrown text-white text-center py-2 px-8 mb-6 w-full border border-white">
+                  <h2 className="text-xl font-bold">{tier.name}</h2>
+                </div>
+                <ul className="space-y-4 mb-6">
+                  {tier.title && <li className="text-base font-semibold"> <bdi> {tier.title} :</bdi></li>}
+                  {tier.features.map((feature, fIndex) => (
+                    <li key={fIndex} className="text-base list-disc" style={{ direction: 'rtl' }}>
+                      {feature}
+                    </li>
+                  ))}
+                </ul>
+                <div className="text-2xl font-bold text-center mt-auto">
+                  {tier.price}
+                </div>
               </div>
             </div>
           </div>
-          </div>
         ))}
-        </div>
+      </div>
     </div>
   );
 };
+
 export default Subscriptions;
